a566a9ac4009fd82e487520f9cf3d3b4a19c548b
Gareth Hind <garethjhind@gmail.com>
2021-07-28 18:05:07 +1000

    Next Link & Router

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,9 @@
-import Link from "next/link";
-
 export default function About() {
   return (
     <div>
       <h1>About</h1>
       <p>This is an app to find the latest DJ and other musical events</p>
       <p>Version: 1.0.0</p>
-      <Link href="/">Home</Link>
     </div>
   );
 }
