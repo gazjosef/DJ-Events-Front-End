d978bb85da3a7a792f27dc5978096887be6f30a5
Gareth Hind <garethjhind@gmail.com>
2021-07-31 14:34:25 +1000

    Searching Page Using Filters

--- pages/events/search.js
+++ pages/events/search.js
@@ -1,11 +1,17 @@
+import qs from "qs";
+import { useRouter } from "next/router";
+import Link from "next/link";
 import Layout from "@/components/Layout";
 import EventItem from "@/components/EventItem";
 import { API_URL } from "@/config/index";
 
 export default function SearchPage({ events }) {
+  const router = useRouter();
+
   return (
-    <Layout>
-      <h1>Events</h1>
+    <Layout title="Search Results">
+      <Link href="/events">Go Back</Link>
+      <h1>Search Results for {router.query.term}</h1>
       {events.length === 0 && <h3>No events to show</h3>}
 
       {events.map((evt) => (
@@ -16,7 +22,17 @@ export default function SearchPage({ events }) {
 }
 
 export async function getServerSideProps({ query: { term } }) {
-  const res = await fetch(`${API_URL}/events?name_contains=${term}`);
+  const query = qs.stringify({
+    _where: {
+      _or: [
+        { name_contains: term },
+        { performers_contains: term },
+        { description_contains: term },
+        { venue_contains: term },
+      ],
+    },
+  });
+  const res = await fetch(`${API_URL}/events?${query}`);
   const events = await res.json();
 
   return {
