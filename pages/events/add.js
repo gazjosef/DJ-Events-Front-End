f52a9c26a5d7025b920e29cc07b08bd909a69d04
Gareth Hind <garethjhind@gmail.com>
2021-07-31 18:30:22 +1000

    Add Event Page

--- pages/events/add.js
+++ pages/events/add.js
@@ -1,9 +1,52 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+import styles from "@/styles/Form.module.css";
 
 export default function AddEventPage() {
+  const [values, setValues] = useState({
+    name: "",
+    performers: "",
+    venue: "",
+    address: "",
+    date: "",
+    time: "",
+    description: "",
+  });
+
+  const router = useRouter();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
+  const handleInputChange = (e) => {
+    const { name, values } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
   return (
     <Layout title="Add New Event">
+      <Link href="/events">Go Back</Link>
       <h1>Add Event</h1>
+
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div className={styles.grid}>
+          <div>
+            <label htmlFor="name">Event Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              values={values.name}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+      </form>
     </Layout>
   );
 }
