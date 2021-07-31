47fbb0b5658484d79c06746c9ac54d7a2880e196
Gareth Hind <garethjhind@gmail.com>
2021-07-31 14:22:48 +1000

    Searching Page Using Filters

---
+++ pages/events/search.js
@@ -0,0 +1,25 @@
+import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import { API_URL } from "@/config/index";
+
+export default function SearchPage({ events }) {
+  return (
+    <Layout>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+    </Layout>
+  );
+}
+
+export async function getServerSideProps({ query: { term } }) {
+  const res = await fetch(`${API_URL}/events?name_contains=${term}`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+  };
+}
