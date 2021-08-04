7b46569d9d0b24976ac4781c43facb1942c68c82
Gareth Hind <garethjhind@gmail.com>
2021-08-04 15:36:19 +1000

    AuthContext

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,12 @@
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+import { useContext } from "react";
 import Link from "next/link";
 import Search from "./Search";
+import AuthContext from "@/context/AuthContext";
 import styles from "@/styles/Header.module.css";
 
 export default function Header() {
+  const { user, logout } = useContext(AuthContext);
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
