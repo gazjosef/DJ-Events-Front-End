ca8c97f23db41737c9c0e21268046d7ba68e8766
Gareth Hind <garethjhind@gmail.com>
2021-08-06 13:15:39 +1000

    Store JWT In Server HttpOnly Cookie

--- pages/api/login.js
+++ pages/api/login.js
@@ -19,7 +19,17 @@ export default async (req, res) => {
     const data = await strapiRes.json();
 
     if (strapiRes.ok) {
-      // @todo - Set cookie
+      // Set Cookie
+      res.setHeader(
+        "Set-Cookie",
+        cookie.serialize("token", data.jwt, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV !== "development",
+          maxAge: 60 * 60 * 24 * 7, // 1 Week
+          sameSite: "strict",
+          path: "/",
+        })
+      );
       res.status(200).json({ user: data.user });
     } else {
       res
