2d0675535e4f5a0ee617eb39e83d2d19f0fda355
Gareth Hind <garethjhind@gmail.com>
2021-08-09 13:45:00 +1000

    Display Dashboard Events

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,12 +1,20 @@
 import { parseCookies } from "@/helpers/index";
 import Layout from "@/components/Layout";
+import DashboardEvent from "@/components/DashboardEvent";
 import { API_URL } from "@/config/index";
+import styles from "@/styles/Dashboard.module.css";
 
 export default function DashboardPage({ events }) {
-  console.log(events);
   return (
     <Layout title="User Dashboard">
-      <h1>Dashboard</h1>
+      <div className={styles.dash}>
+        <h1>Dashboard</h1>
+        <h3>My Events</h3>
+
+        {events.map((evt) => (
+          <DashboardEvent key={evt.id} evt={evt} />
+        ))}
+      </div>
     </Layout>
   );
 }
